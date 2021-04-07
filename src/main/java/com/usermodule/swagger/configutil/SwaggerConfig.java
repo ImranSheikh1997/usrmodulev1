8c6ba8f8493d3d94fe85be2e876fac88d086a604
Imran Sheikh <imran.sheikh@globalvoxinc.com>
2021-04-07 11:52:54 +0530

    Swagger Working Perfectly url: http://localhost:8080/swagger-ui.html

--- src/main/java/com/usermodule/swagger/configutil/SwaggerConfig.java
+++ src/main/java/com/usermodule/swagger/configutil/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.usermodule.swagger.configutil;
 
+
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -18,31 +20,40 @@ import java.util.Optional;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig  {
-
+public class SwaggerConfig {
 
     @Bean
-    public Docket api(){
-
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)//
-        .select()//
-        .apis(RequestHandlerSelectors.basePackage("com.usermodule"))//
-        //.paths(Predicates.not(PathSelectors.regex("/error.")))//
-        .build()//
-        .apiInfo(metadata())//
-        .useDefaultResponseMessages(false)//
-        .securitySchemes(Collections.singletonList(apiKey()))
-        .securityContexts(Collections.singletonList(securityContext()))//
-        .tags(new Tag("User","Crud Operations on User"))//
-        .genericModelSubstitutes(Optional.class);
+                .select()//
+                .apis(RequestHandlerSelectors.any())//
+                .paths(Predicates.not(PathSelectors.regex("/error")))//
+                .build()//
+                .apiInfo(metadata())//
+                .useDefaultResponseMessages(false)//
+                .securitySchemes(Collections.singletonList(apiKey()))
+                .securityContexts(Collections.singletonList(securityContext()))
+                .tags(new Tag("users", "Operations about users"))//
+                .genericModelSubstitutes(Optional.class);
 
+    }
+
+    private ApiInfo metadata() {
+        return new ApiInfoBuilder()//
+                .title("JSON Web Token Authentication API")//
+                .description("This is a sample User Registration Service with JWT. You can find out more about JWT at [https://jwt.io/](https://jwt.io/). ")//
+                //For this sample, you can use the `admin` or `client` users (password: admin and client respectively) to test the authorization filters. Once you have successfully logged in and obtained the token, you should click on the right top button `Authorize` and introduce it with the prefix "Bearer ".
+                .version("1.0.0")//
+                .license("GlobalVox License").licenseUrl("https://www.globalvoxinc.com/")//
+                .contact(new Contact("Imran", "https://www.globalvoxinc.com/", "imran.sheikh@globalvoxinc.com"))//
+                .build();
     }
 
     private ApiKey apiKey() {
-        return new ApiKey("Authorization","Authorization","Header");
+        return new ApiKey("Authorization", "Authorization", "header");
     }
 
-    private springfox.documentation.spi.service.contexts.SecurityContext securityContext() {
+    private SecurityContext securityContext() {
         return SecurityContext.builder()
                 .securityReferences(defaultAuth())
                 .forPaths(PathSelectors.any())
@@ -50,19 +61,10 @@ public class SwaggerConfig  {
     }
 
     private List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("Global","Access Everything");
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("Authorization",authorizationScopes));
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
     }
 
-    private ApiInfo metadata() {
-        return new ApiInfoBuilder()//
-        .title("User Module Using MYSQL & JSON Web Token Authentication API")//
-        .description("This is simple UserModule Service Having Registration Form, Log in Form, and Crud Operations")//
-        .version("1.0.0")//
-        .license("GlobalVox PVT ltd").licenseUrl("https://www.globalvoxinc.com")//
-        .contact(new Contact("Imran","https://www.globalvoxinc.com/contact-us","imran.sheikh@globalvoxinc.com"))//
-        .build();
-    }
 }
